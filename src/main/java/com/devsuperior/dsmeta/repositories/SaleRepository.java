878cbf3044476e460ca6766f5c74caf2d96a3bec
Gabriel_Sousa <gabriel.sist.inf@gmail.com>
2024-12-27 21:26:44 -0300

    test finish

--- src/main/java/com/devsuperior/dsmeta/repositories/SaleRepository.java
+++ src/main/java/com/devsuperior/dsmeta/repositories/SaleRepository.java
@@ -1,9 +1,26 @@
 package com.devsuperior.dsmeta.repositories;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+import java.time.LocalDate;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import com.devsuperior.dsmeta.dto.SaleMinDTO;
+import com.devsuperior.dsmeta.dto.SaleSummaryDTO;
 import com.devsuperior.dsmeta.entities.Sale;
 
 public interface SaleRepository extends JpaRepository<Sale, Long> {
+
+	@Query("SELECT new com.devsuperior.dsmeta.dto.SaleMinDTO(obj.id, obj.date, obj.amount, obj.seller.name) FROM Sale obj "
+			+ "WHERE obj.date BETWEEN :iniDate AND :finDate "
+			+ "AND UPPER(obj.seller.name) LIKE UPPER(CONCAT('%', :sellerName, '%'))")
+	Page<SaleMinDTO> searchReport( LocalDate iniDate, LocalDate finDate, String sellerName, Pageable pageable);
+	
+	@Query("SELECT new com.devsuperior.dsmeta.dto.SaleSummaryDTO(obj.seller.name, SUM(obj.amount)) FROM Sale obj "
+			+ "WHERE obj.date BETWEEN :iniDate AND :finDate "
+			+ "GROUP BY obj.seller.name")
+	Page<SaleSummaryDTO> searchSummary(LocalDate iniDate, LocalDate finDate, Pageable pageable);
 
 }
--- src/main/java/com/devsuperior/dsmeta/services/SaleService.java
+++ src/main/java/com/devsuperior/dsmeta/services/SaleService.java
@@ -1,23 +1,74 @@
 package com.devsuperior.dsmeta.services;
 
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import com.devsuperior.dsmeta.dto.SaleMinDTO;
+import com.devsuperior.dsmeta.dto.SaleSummaryDTO;
 import com.devsuperior.dsmeta.entities.Sale;
 import com.devsuperior.dsmeta.repositories.SaleRepository;
 
 @Service
 public class SaleService {
-
+	private static DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+	
 	@Autowired
 	private SaleRepository repository;
-	
+
 	public SaleMinDTO findById(Long id) {
 		Optional<Sale> result = repository.findById(id);
 		Sale entity = result.get();
 		return new SaleMinDTO(entity);
 	}
+
+	public Page<SaleMinDTO> searchReport(String iniDate, String finDate, String name, Pageable pageable) {
+		LocalDate finDate1 = null;
+		LocalDate iniDate1 = null;
+
+		if (finDate.isEmpty()) {
+			finDate1 = LocalDate.ofInstant(Instant.now(), ZoneId.systemDefault());
+		} else {
+			finDate1 = LocalDate.parse(finDate, formatter);
+		}
+
+		if (iniDate.isEmpty()) {
+			iniDate1 = finDate1.minusYears(1);
+		} else {
+			iniDate1 = LocalDate.parse(iniDate, formatter);
+		}
+		
+		Page<SaleMinDTO> dto = repository.searchReport(iniDate1, finDate1, name, pageable);
+
+		return dto;
+	}
+
+	public Page<SaleSummaryDTO> searchSummary(String iniDate, String finDate, Pageable pageable) {
+		LocalDate finDate1 = null;
+		LocalDate iniDate1 = null;
+		
+		if (iniDate.isEmpty()) {
+			finDate1 = LocalDate.ofInstant(Instant.now(), ZoneId.systemDefault());
+		} else {
+			finDate1 = LocalDate.parse(finDate, formatter);
+		}
+
+		if (iniDate.isEmpty()) {
+			iniDate1 = finDate1.minusYears(1);
+		} else {
+			iniDate1 = LocalDate.parse(iniDate, formatter);
+		}
+		
+		Page<SaleSummaryDTO> dto = repository.searchSummary(iniDate1, finDate1, pageable);
+		return dto;
+	}
+	
+	
 }
